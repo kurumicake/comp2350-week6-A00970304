ade279309ac2b14623f4e0b624668842acdca5e2
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-02-28 19:23:59 -0800

    again

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
+//Define the include function for absolute file name
+global.base_dir = __dirname;
+global.abs_path = function(path) {
+	return base_dir + path;
+}
+global.include = function(file) {
+	return require(abs_path('/' + file));
+}
+
+
 const express = require('express');
-const app = express();
 const database = include('databaseConnection');
 const router = include('routes/router');
 
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-
-// Place these lines before your routes to parse the body of incoming requests
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Now add your router
-app.use('/', router);
-
-// Static files middleware
-app.use(express.static(__dirname + "/public"));
-
 async function printMySQLVersion() {
-    let sqlQuery = `SHOW VARIABLES LIKE 'version';`;
-    try {
-        const results = await database.query(sqlQuery);
-        console.log("Successfully connected to MySQL");
-        console.log(results[0]);
-        return true;
-    } catch (err) {
-        console.log("Error getting version from MySQL", err);
-        return false;
-    }
+	let sqlQuery = `
+		SHOW VARIABLES LIKE 'version';
+	`;
+	
+	try {
+		const results = await database.query(sqlQuery);
+		console.log("Successfully connected to MySQL");
+		console.log(results[0]);
+		return true;
+	}
+	catch(err) {
+		console.log("Error getting version from MySQL", err);
+		return false;
+	}
 }
 
 const success = printMySQLVersion();
 
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use('/',router);
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extended: true}));
+
 app.listen(port, () => {
-    console.log("Node application listening on port " + port);
-});
+	console.log("Node application listening on port "+port);
+}); 
+
+
+
